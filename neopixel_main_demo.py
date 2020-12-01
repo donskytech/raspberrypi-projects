47ab3f85b9d8a7e1281d8748c607975de8cc8be2
donskytech <donsky.iot.tech@gmail.com>
2020-12-01 16:38:31 +0800

    Fix error

--- neopixel_main_demo.py
+++ neopixel_main_demo.py
@@ -4,6 +4,6 @@ from ws2812b_neopixel_luma_led_matrix.neopixel_demo import show_effect
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Input effects for ws2812b/neopixel")
     parser.add_argument('effect', type=str, help="Input Effect Type")
-    effect = parser.parse_args()
-    print(f"Received command line effect argument : {effect}")
-    show_effect(effect);
+    args = parser.parse_args()
+    print(f"Received command line effect argument : {args.effect}")
+    show_effect(args.effect);
