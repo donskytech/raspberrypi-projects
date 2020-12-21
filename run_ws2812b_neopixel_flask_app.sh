5853736bb482bff87f88d8b001fb33fa5655c7fa
donskytech <donsky.iot.tech@gmail.com>
2020-12-21 18:12:29 +0800

    Create runnable bash script

--- run_ws2812b_neopixel_flask_app.sh
+++ run_ws2812b_neopixel_flask_app.sh
@@ -4,4 +4,6 @@ export PYTHONPATH="$PWD:"
 
 mkdir -p /var/log/gunicorn/
 
+echo "Starting WS2812B Neopixel Flask Application..."
+
 gunicorn -w 1 -b 0.0.0.0:5000 ws2812b_neopixel_flask_final.app:app --error-logfile /var/log/gunicorn/error.log --access-logfile /var/log/gunicorn/access.log --capture-output --log-level debug
