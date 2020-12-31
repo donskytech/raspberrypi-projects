5d1cbed5211f8c4a432fd8a6c57b10654915063f
donskytech <donsky.iot.tech@gmail.com>
2020-12-31 13:25:54 +0800

    Remove gevent

--- ws2812b_neopixel_flask/app.py
+++ ws2812b_neopixel_flask/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify
-from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
@@ -10,6 +9,4 @@ def index():
 
 
 if __name__ == "__main__":        # on running python app.py
-    # app.run(host='192.168.100.10', port='8080', debug='true', threaded='true')                     # run the flask app
-    http_server = WSGIServer(('0.0.0.0', 8081), app)
-    http_server.serve_forever()
+    app.run(host='0.0.0.0', port='8080', debug='true') # run the flask app
