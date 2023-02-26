bb3147b765422ac9d7a270669de4c673ae9d53a8
donskytech <donsky.iot.tech@gmail.com>
2023-02-26 20:16:01 +0800

    Initial Code

---
+++ dht22/old_dht_simpletest.py
@@ -0,0 +1,24 @@
+import Adafruit_DHT
+
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT22
+
+
+# Example using a Raspberry Pi with DHT sensor
+# connected to GPIO18.
+pin = 18
+
+while True:
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
